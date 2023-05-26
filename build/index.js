557d87811afbac9ae1831e26e6490e475f681f9a
Varga Miklós Máté <varga.miklos.mate@MAIL.LOVASSY.HU>
2023-05-26 11:27:59 +0200

    sending files function

--- build/index.js
+++ build/index.js
@@ -1,10 +1,15 @@
 import express from "express";
-const app = express();
+import { WebSocketServer } from "ws";
 const PORT = 80;
+const WebSocketPort = 443;
+const app = express();
+const SocketServer = new WebSocketServer({ port: WebSocketPort });
+const loaded = {};
 app.get("*", (req, res) => {
-    console.log("ky");
 });
 app.listen(PORT, () => {
     console.log("Server started on port: " + PORT);
 });
+function SendFile(req, res) {
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import express  from "express"
-
-const app = express()
+import { WebSocket, WebSocketServer } from "ws"
+import * as fs from "fs"
+import Promise from "promise"
 
 const PORT = 80
+const WebSocketPort = 443
+
+const app = express()
+const SocketServer = new WebSocketServer({port: WebSocketPort})
+
+const loaded = {}
 
 app.get("*", (req, res) => {
-    console.log("ky")
+    
 })
 
 app.listen(PORT, () => {
     console.log("Server started on port: " + PORT)
 })
 
+
+function SendFile(req : express.Request, res : express.Response){
+
+}
