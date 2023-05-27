c54ad0b77ec65bb0ea6c974b86e635405865b504
Varga Miklós <vargamiki53@gmail.com>
2023-05-27 09:45:26 +0200

    added some loggers

--- build/index.js
+++ build/index.js
@@ -4,9 +4,12 @@ const WebSocketPort = 443;
 const Pending = {};
 const wss = new ws.WebSocketServer({ port: WebSocketPort });
 wss.on("connection", (ws) => {
+    console.log("connected");
     ws.on("message", (data, isBinary) => {
+        console.log("message sent");
         wss.clients.forEach((client) => {
             if (client.readyState == ws.OPEN) {
+                console.log("sent to client");
                 client.send(JSON.stringify(data.toString));
             }
         });
--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,15 @@ const Pending = {
 const wss = new ws.WebSocketServer({port: WebSocketPort})
 
 wss.on("connection", (ws) => {
+    console.log("connected")
+
     ws.on("message", (data, isBinary) => {
+        console.log("message sent")
+
         wss.clients.forEach((client) => {
             if(client.readyState == ws.OPEN){
+                console.log("sent to client")
+
                 client.send(JSON.stringify(data.toString))
             }
         })
