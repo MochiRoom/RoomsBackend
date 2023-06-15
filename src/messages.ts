697caee6d092d939be5b86f8bf5dfd96322850b3
miki <vargamiki53@gmail.com>
2023-06-15 19:37:43 +0200

    removed html support

--- src/messages.ts
+++ src/messages.ts
@@ -1,13 +1,13 @@
 export class Message { 
     public data : string;
 
-    public author : string;
+    public author : number;
 
     public room : number;
 
     public date : number;
 
-    constructor (tData : string, tAuthor : string, tRoom : number, tDate : number){
+    constructor (tData : string, tAuthor : number, tRoom : number, tDate : number){
         this.data = tData
         this.author = tAuthor
         this.room = tRoom
