82c1f5e6959a45d69145d7ab2d4a9b9fbdda5f7a
miki <vargamiki53@gmail.com>
2023-06-18 14:59:31 +0200

    fixed logger

--- src/websocket.ts
+++ src/websocket.ts
@@ -12,26 +12,28 @@ export function connection(ws : ws.WebSocket){
         
         var tMessage : Message = JSON.parse(data.toString())
         
-        if (!Rooms.has(tMessage.room))
-        Rooms.set(tMessage.room, new Room(tMessage.room))
+        if (!Rooms.has(tMessage.room)){
+            Rooms.set(tMessage.room, new Room(tMessage.room))
+        }
+
         Rooms.get(tMessage.room).messages.push(new Message(tMessage.data, tMessage.author, tMessage.room, tMessage.date))
         
         let t = 0
-
+        
         //sending the message to each client connected
         wss.clients.forEach((client) => {
             
             if(client.readyState == ws.OPEN){
-
+                
                 t++
-
+                
                 var Send = JSON.stringify(Rooms.get(tMessage.room).messages[Rooms.get(tMessage.room).messages.length - 1 ])
-
+                
                 client.send(Send)
             }
             
         })
-
-        logger.Logger("Message Receveice", ["The message was: " + tMessage.data, "From user: " + tMessage.author, "And was sent out to: " + t + " amount of clients"])
+        
+        logger.Logger("Message Receveice", ["The message was: " + tMessage.data, "From user: " + tMessage.author.name + " " + tMessage.author.id, "And was sent out to: " + t + " amount of clients"])
     })
 }
